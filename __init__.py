26705e42c63a21848215c6f015a0ce560c49dccd
Devin Cornell <devinj.cornell@gmail.com>
2016-12-13 19:02:27 -0800

    Updated name of repo in __init__

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 
-from network_timeseries.NetTS import *
+from networkxtimeseries.NetTS import *
 
-from ut_measGraph import *
-from ut_measNodes import *
-from ut_ntsfiles import *
+
 
 if __name__ == '__main__':
+
+	from ut_measGraph import *
+	from ut_measNodes import *
+	from ut_ntsfiles import *
 
 	# run unit tests
 	ut_measGraph()
