483a5d9ab6df6b465c22bd0eb174db40c0bc8094
Devin Cornell <devinj.cornell@gmail.com>
2016-12-13 23:27:40 -0800

    Fleshed out XML writer code. Still need to write change detection algorithm.

--- xgmml.py
+++ xgmml.py
@@ -31,34 +31,63 @@ def get_value_changes(ds):
 	'''
 	changes = [(ds.index[0],ds[ds.index[0]])]
 
-	pass
+	return changes
+
 
 ##### File Output Functions #####
+
 def build_xgmml_file(f,gdf,ndf,edf):
 	''' This function builds the xml file given the file object f,
     a graph df, node df, and edge df. First it will look at when 
 	attributes change, and then use that to decide when to add an 
 	attribute tag.
 	'''
-	f.write('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>')
-	f.write('<!-- Created using the networkxtimeseries library for python. -->')
-	f.write('<graph label="testDD5" directed="0">')
+	f.write(header_str)
+	f.write(graph_start_str)
+	t0 = gdf.index[0]
+	tf = gdf.index[-1]
 
-	for n in ndf.columns:
-		f.write('\t<node label="node_1" id="1" start="0" end="100">')
-		changes = get_value_changes(ndf[n])
-		for c in changes:
-			f.write('\t\t<att name="size" type="real" value="82" start="0" end="100"/>')
-		f.write('\t</node>')
+	i = 0
+	for n in ndf.columns.get_level_values(0):
+		values = {'label':str(n),'id':str(i),'start':t0,'end':t0}
+		f.write(node_start.format(**values))
+		
+		for a in ndf.columns.get_level_values(1):
+			changes = get_value_changes(ndf.loc[:,[n,a]])
+			for c in range(len(changes[:-1])):
+				values = {'name':a,'type':'real','value':changes[c][1],'start':changes[c][0],'end':changes[c+1][0]}
+				f.write(node_start_str.format(**values))
+		i += 1
+		f.write(node_end_str)
 
-	for e in edf.columns:
-		f.write('\t<edge label="edge_4_6" source="4" target="6" start="0" end="79">')
-		changes = get_value_changes(ndf[e])
-		for c in changes:
-			f.write('\t\t<att name="weight" type="real" value="127.764" start="0" end="22"/>')
-		f.write('\t</node>')
+	for u in edf.columns.get_level_values(0):
+		for v in edf.columns.get_level_values(1):
+			values = {'label':'(%s,%s)'%(str(u),str(v)),'source':str(u),'target':str(v),'start':ts[0],'end':ts[-1]}
+			f.write(edge_start_str.format(**values))
+			
+			for a in ndf.columns.get_level_values(2):
+				changes = get_value_changes(ndf.loc[:,(u,v,a)])
+				for c in range(len(changes[:-1])):
+					values = {'name':a,'type':'real','value':changes[c][1],'start':changes[c][0],'end':changes[c+1][0]}
+					f.write(attr_str.format(**values))
+			f.write(edge_end_str)
 
-	f.write('</graph>')
+	f.write(graph_end_str)
+
+	return
 
 
+##### File Output Strings #####
+header_str = '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\r\n
+<!-- Created using the networkxtimeseries library for python. -->\r\n'''
 
+graph_start_str = '<graph label="testDD5" directed="0">\r\n'
+graph_end_str = '</graph>'
+
+node_start_str = '\t<node label="{label}" id="{id}" start="{start}" end="{end}">\r\n'
+node_end_str = '\t</node>'
+
+edge_start_str = '\t<edge label="edge_4_6" source="4" target="6" start="0" end="79">\r\n'
+edge_end_str = '</edge>'
+
+attr_str = '\t\t<att name="{name}" type="{type}" value="{value}" start="{start}" end="{end}"/>\r\n'
