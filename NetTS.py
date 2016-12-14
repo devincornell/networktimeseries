f884a942fc2581398a6c591308fdd3191428a869
Devin Cornell <devinj.cornell@gmail.com>
2016-12-14 10:19:26 -0800

    Added ability to add static attributes and ensure every graph has same edges/nodes depending on selected type. I'm going to remove this in the next commit though, because I think I'm over-complicating things.

--- NetTS.py
+++ NetTS.py
@@ -46,29 +46,24 @@ class NetTS:
 		return
 
 	def save_xgmml(self, filename):
-		gdf = self.measure(xgmml.meas_graph_attr, meas_obj='graph', parallel=False)
-		ndf = self.measure(xgmml.meas_node_attr, meas_obj='nodes', parallel=False)
-		edf = self.measure(xgmml.meas_edge_attr, meas_obj='edges', parallel=False)
-		with open(filename) as f:
-			xgmml.build_xgmml_file(f,gdf,ndf,edf)
+		raise
+		with open(filename,'w') as f:
+			xgmml.build_xgmml_file(f,ndf,edf)
 
 		return
 
-	def __init__(self, ts, nodes=None, edges=None):
+	def __init__(self, ts, nodes=None, edges=None, type='static_nodes'):
 		ts = list(ts) # ts is a timeseries list
 		if nodes is not None: nodes = list(nodes) # nodes is a list of node names
 		if edges is not None: edges = list(edges) # edges is a list of edges
 
 		# set timeseries type
-		if nodes is None:
-			self.type = 'nonstatic'
-			print('Error - choose at least a set of nodes in NetTs init.')
+		if type == 'static_nodes' or type == 'static_structure':
+			self.type = type
+		elif type == 'dynamic':
+			print('Error - choose at least a set of nodes in NetTS init.')
+			print('Support for dynamic nodes is not supported.')
 			exit()
-
-		elif nodes is not None and edges is None:
-			self.type = 'static_nodes'
-		elif nodes is not None and edges is not None:
-			self.type = 'static_structure'
 		else:
 			print('network type not recognized in NetTs init.')
 			exit()
@@ -97,6 +92,46 @@ class NetTS:
 					self[t].add_edge(e)
 		else:
 			self.edges = list()
+
+	def setStaticAttr(self, attr_dict, type='node'):
+		''' Set a set of static attributes to either nodes or edges.'''
+		for o,odata in attr_dict.items():
+			for attr,val in odata.items():
+				if type == 'node':
+					self.static_node_attr[o][attr] = val
+				elif type == 'edge':
+					self.static_edge_attr[o][attr] = val
+		return
+
+	def update(self):
+		''' This function will ensure consistency across time periods
+		and also keep track of dynamic vs static properties.'''
+		# assign all node/edge properties to be dynamic unless they are static
+		# get unique node set
+		
+		# update self.nodes, self.edges to contain all possible unique edges
+		nodeset = set()
+		edgeset = set()
+		for t in self.ts:
+			nodeset.union(self[t].nodes())
+			edgeset.union(self[t].edges())
+		self.nodes = list(nodeset)
+		self.edges = list(edgeset)
+
+		# ensure every node/edge has a static _tag attribute
+		self.static_node_attr = {n:{'_tag':str(n)} for n in self.nodes}
+		self.static_edge_attr = {e:{'_tag':str(e)} for e in self.edges}
+
+		# add nodes/edges to every graph as needed
+		if type == 'static_nodes' or type == 'static_structure':
+			for t in self.ts:
+				for n in self.nodes:
+					if n not in self[t].nodes:
+						self[t].add_node(n,attr_dict={'_tag':str(n)})
+				if type == 'static_structure':
+					for e in self.edges:
+						if e not in self[t].edges():
+							self[t].add_edge(e,attr_dict={'_tag':str(e)})
 
 	##### Set Graph, Node, and Edge Attributes #####
 	def setGraphAttr(self, t, attrName, gdata):
@@ -144,7 +179,7 @@ class NetTS:
 		return outNet
 
 	##### Measure Properties of Graphs Over Time #####
-	def measure(self, measFunc, meas_obj='graph', addtnlArgs=list(), parallel=False):
+	def time_measure(self, measFunc, meas_obj='graph', addtnlArgs=list(), parallel=False):
 		''' Returns a multiindex dataframe of measurements for all nodes at each 
 		point in time. measFunc should expect a graph object and return a 
 		dictionary with (node,attr) as keys. Output: The index will be a timeseries, 
@@ -152,13 +187,15 @@ class NetTS:
 		'''
 		# error checking
 		if not (meas_obj == 'graph' or meas_obj == 'nodes' or meas_obj == 'edges'): raise
-		try: trymeas = measFunc(self.nts[0], *addtnlArgs)
-		except: print('measFunc threw an error:\r\n', sys.stderr[0]); exit()
+		trymeas = measFunc(self.nts[0], *addtnlArgs)
 		try: dict(trymeas)
 		except TypeError: print('Error in measure(): measFunc should return a dict'); exit()
 		if meas_obj == 'nodes' or meas_obj == 'edges':
 			try: [list(m) for m in trymeas];
 			except TypeError: print('Error in measure(): measFunc keys should follow (node,attr).'); exit()
+
+		if len(trymeas) == 0: # return empty dataframe
+			return pd.DataFrame()
 
 		if meas_obj == 'graph':
 			cols = list(trymeas.keys())
@@ -175,13 +212,12 @@ class NetTS:
 		else:
 			with multiprocessing.Pool(processes=4) as p:
 				meas = p.map(self.thread_measure, tdata)
-
 		for t,mdf in meas:
 			df.loc[[t],:] = mdf
 
 		return df
 
-	def thread_measure(self, dat):
+	def thread_time_measure(self, dat):
 		''' This is a thread function that will call measFunc on each
 		network in the timeseries. measFunc is responsible for returning
 		a dictionary with (node,attr) keys.
@@ -191,3 +227,47 @@ class NetTS:
 		
 		return t,pd.DataFrame([meas,],index=[t,],columns=cols)
 
+	def getStatNodeAttr(self):
+		self.update()
+		return self.static_node_attr
+	
+	def getStatEdgeAttr(self):
+		self.update()
+		return self.static_edge_attr
+
+	def getDynNodeAttr(self):
+		ndf = self.time_measure(xgmml.meas_node_attr, meas_obj='nodes', parallel=False)
+		pass
+
+	def getDynEdgeAttr(self):
+		edf = self.time_measure(xgmml.meas_edge_attr, meas_obj='edges', parallel=False)
+		pass
+
+
+##### Standalone Measurement Functions #####
+''' These functions are used in the class but not explicitly class 
+members.
+'''
+
+def meas_node_attr(G):
+	meas = dict()
+	attr = G.nodes(data=True)
+	for a,val in attr:
+		attr = nx.get_node_attributes(G,a)
+		meas.update({(n,a):val for n in G.nodes()})
+	if len(meas) == 0:
+		meas.update({(n,'_tag'):str(n) for n in G.nodes()})
+
+	return meas
+
+def meas_edge_attr(G):
+	meas = dict()
+	e = G.edges()[0]
+	attr = G.get_edge_data(e[0],e[1])
+	for a,val in attr.items():
+		attr = nx.get_edge_attributes(G,a)
+		meas.update({(e[0],e[1],a):val for e in G.edges()})
+	if len(attr) == 0:
+		meas.update({(e[0],e[1],_tag):str(e) for e in edges()})
+
+	return meas
