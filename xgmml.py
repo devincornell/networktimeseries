ab42752ea9deb6cf26ba1bf667e4bb69de86a1a1
Devin Cornell <devinj.cornell@gmail.com>
2016-12-15 08:13:27 -0800

    Finished basic code to output nodes, edges, and attributes.

--- xgmml.py
+++ xgmml.py
@@ -21,43 +21,44 @@ def build_xgmml_file(f,ndf,edf):
 	attributes change, and then use that to decide when to add an 
 	attribute tag.
 	'''
-	f.write(header_str)
-	f.write(graph_start_str)
+	
 	t0 = edf.index[0]
 	tf = edf.index[-1]
 
-	for n in ndf.columns.levels[0]:
+	f.write(header_str)
+	f.write(graph_start_str.format(label='mygraph'))
+
+	for n in list(set([x[0] for x in ndf.columns])):
+				
 		values = {'label':str(n),'id':str(n),'start':t0,'end':tf}
 		f.write(node_start_str.format(**values))
 
-		for a in ndf.loc[:,(n,slice(None))].columns.levels[1]:
-			changes = get_value_changes(ndf.loc[:,(n,a)])
-			
-			# new attr tag for every change in an attribute
-			if len(changes) == 1:
-				values = {'name':a,'type':'real','value':changes[0][1],'start':t0,'end':tf}
+		for attr in [x[1] for x in filter(lambda x:x[0]==n,ndf.columns)]:
+			changes = get_value_changes(ndf.loc[:,(n,attr)])
+			for c in range(len(changes[:-1])):
+				values = {'name':attr,'type':'real','value':changes[c][1],'start':changes[c][0],'end':changes[c+1][0]}
 				f.write(attr_str.format(**values))
-			else:
-				for c in range(len(changes[:-1])):
-					values = {'name':a,'type':'real','value':changes[c][1],'start':changes[c][0],'end':changes[c+1][0]}
-					f.write(attr_str.format(**values))
+			if len(changes) == 1:
+				values = {'name':attr,'type':'real','value':changes[0][1],'start':t0,'end':tf}
+				f.write(attr_str.format(**values))
+
 		f.write(node_end_str)
 
-	ind = edf.loc[:,(u,slice(None),slice(None))].columns
-	for u in ndf.columns.levels[0]:
-		vind = filter(lambda x: x[0]==u,ind)
-		for v in vind:
-			values = {'label':'(%s,%s)'%(str(u),str(v)),'source':str(u),'target':str(v),'start':t0,'end':tf}
-			f.write(edge_start_str.format(**values))
-			
-			
-			aind = filter(lambda x: x[0]==u and x[1]==v, vind)
-			for a in aind:
-				changes = get_value_changes(edf.loc[:,(u,v,a)])
-				for c in range(len(changes[:-1])):
-					values = {'name':a,'type':'real','value':changes[c][1],'start':changes[c][0],'end':changes[c+1][0]}
-					f.write(attr_str.format(**values))
-			f.write(edge_end_str)
+	for u,v in list(set([x[:2] for x in edf.columns])):
+		
+		values = {'label':'(%s,%s)'%(str(u),str(v)),'source':str(u),'target':str(v),'start':t0,'end':tf}
+		f.write(edge_start_str.format(**values))
+
+		for attr in [x[2] for x in filter(lambda x:x[:2] == (u,v),edf.columns)]:
+			changes = get_value_changes(edf.loc[:,(u,v,attr)])
+			for c in range(len(changes[:-1])):
+				values = {'name':attr,'type':'real','value':changes[c][1],'start':changes[c][0],'end':changes[c+1][0]}
+				f.write(attr_str.format(**values))
+			if len(changes) == 1:
+				values = {'name':attr,'type':'real','value':changes[0][1],'start':t0,'end':tf}
+				f.write(attr_str.format(**values))
+
+		f.write(edge_end_str)
 
 	f.write(graph_end_str)
 
@@ -68,13 +69,13 @@ def build_xgmml_file(f,ndf,edf):
 header_str = '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <!-- Created using the networkxtimeseries library for python. -->\n\n'''
 
-graph_start_str = '<graph label="testDD5" directed="0">\n'
+graph_start_str = '<graph label="{label}" directed="0">\n'
 graph_end_str = '</graph>\n'
 
 node_start_str = '\t<node label="{label}" id="{id}" start="{start}" end="{end}">\n'
 node_end_str = '\t</node>\n'
 
-edge_start_str = '\t<edge label="edge_4_6" source="4" target="6" start="0" end="79">\n'
+edge_start_str = '\t<edge label="{label}" source="{source}" target="{target}" start="{start}" end="{end}">\n'
 edge_end_str = '\t</edge>\n'
 
 attr_str = '\t\t<att name="{name}" type="{type}" value="{value}" start="{start}" end="{end}"/>\n'
